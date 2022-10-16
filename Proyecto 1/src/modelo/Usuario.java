c30987c5eb9f620ea0dae3fe51a2350cc1a0129f
nicolozanoh <98645826+nicolozanoh@users.noreply.github.com>
2022-10-16 13:55:08 -0500

    arreglo

---
+++ Proyecto 1/src/modelo/Usuario.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public abstract class Usuario {
+	protected String nombreUsuario;
+	protected String contraseña;
+	
+	public abstract String getNombreUsuario();
+	public abstract String getContraseña();
+}
